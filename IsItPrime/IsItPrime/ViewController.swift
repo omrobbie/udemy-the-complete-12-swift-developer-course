f4e207c01cb6df63eb5c3e47cd8dcdefdb7905b6
omrobbie <putu.roby@yahoo.com>
2020-08-21 15:51:30 +0800

    add logic to the app

--- IsItPrime/IsItPrime/ViewController.swift
+++ IsItPrime/IsItPrime/ViewController.swift
@@ -18,5 +18,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnPrimeCheckerTapped(_ sender: Any) {
+        if let numberInput = txtNumber.text {
+            if let numberInt = Int(numberInput) {
+                var isPrime = true
+                var i = 2
+
+                if numberInt == 1 {
+                    isPrime = false
+                }
+
+                while i < numberInt {
+                    if numberInt % i == 0 {
+                        isPrime = false
+                    }
+                    i += 1
+                }
+
+                if isPrime {
+                    lblResult.text = "\(numberInt) is prime!"
+                } else {
+                    lblResult.text = "\(numberInt) is not prime!"
+                }
+            } else {
+                lblResult.text = "Please enter a positive whole number."
+            }
+        }
     }
 }
