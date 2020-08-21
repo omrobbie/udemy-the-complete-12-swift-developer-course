feddcde0a797660b99027bd6139bfbac7f37f5bb
omrobbie <putu.roby@yahoo.com>
2020-08-21 15:45:07 +0800

    setup layout

--- IsItPrime/IsItPrime/ViewController.swift
+++ IsItPrime/IsItPrime/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func btnPrimeCheckerTapped(_ sender: Any) {
     }
 }
