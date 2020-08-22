95b53139a0b52c47d4379d83e2d0b3d411e7795e
omrobbie <putu.roby@yahoo.com>
2020-08-22 11:25:15 +0800

    add logic for save and load data from user defaults

--- WhatsMyNumber/WhatsMyNumber/ViewController.swift
+++ WhatsMyNumber/WhatsMyNumber/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var txtNumber: UITextField!
 
+    private let key = "number"
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+    }
+
+    private func loadData() {
+        if let number = UserDefaults.standard.string(forKey: key) {
+            txtNumber.text = number
+        }
     }
 
     @IBAction func btnSaveTapped(_ sender: Any) {
+        UserDefaults.standard.set(txtNumber.text, forKey: key)
     }
 }
