0feae1ac768f8f284daf170066fc0874059d0554
omrobbie <putu.roby@yahoo.com>
2020-08-22 10:04:06 +0800

    setup layout

--- TimesTables/TimesTables/ViewController.swift
+++ TimesTables/TimesTables/ViewController.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func sliderChange(_ sender: Any) {
+        print(slider.value)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
     }
 }
