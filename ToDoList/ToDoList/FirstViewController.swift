7b8f2aad58e1d69d78554e8c6c7c25bb6347880b
omrobbie <putu.roby@yahoo.com>
2020-08-22 21:16:26 +0800

    * start to do list project *

---
+++ ToDoList/ToDoList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoList
+//
+//  Created by omrobbie on 22/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
