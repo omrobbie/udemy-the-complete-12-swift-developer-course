954bf4635b52546dca7ff197e40d21c330a2ddd0
omrobbie <putu.roby@yahoo.com>
2020-08-24 07:57:51 +0800

    connect layout to controller

--- BackToBach/BackToBach/ViewController.swift
+++ BackToBach/BackToBach/ViewController.swift
@@ -10,7 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderVolume: UISlider!
+    @IBOutlet weak var sliderScrubber: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func sliderVolumeChanged(_ sender: Any) {
+    }
+
+    @IBAction func sliderScrubberChanged(_ sender: Any) {
+    }
+
+    @IBAction func btnPlayTapped(_ sender: Any) {
+    }
+
+    @IBAction func btnPauseTapped(_ sender: Any) {
+    }
+
+    @IBAction func btnStopTapped(_ sender: Any) {
     }
 }
