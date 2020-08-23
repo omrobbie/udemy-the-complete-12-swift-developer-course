3033fd30b5f66992bdeb3116982f9ce7ba2eb15b
omrobbie <putu.roby@yahoo.com>
2020-08-23 19:30:45 +0800

    declare the state for win situations

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     var activePlayer = 1
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+    let winState = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
