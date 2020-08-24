17cdab66ec7e0fc8f8995d4bd02783bc9fbd7d67
omrobbie <putu.roby@yahoo.com>
2020-08-24 11:35:03 +0800

    add sound assets and setup player

--- BackToBach/BackToBach/ViewController.swift
+++ BackToBach/BackToBach/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var sliderVolume: UISlider!
     @IBOutlet weak var sliderScrubber: UISlider!
 
+    private let audioPath = Bundle.main.path(forResource: "song", ofType: "mp3")
+    private var player = AVAudioPlayer()
+    private var timer = Timer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPlayer()
+    }
+
+    private func setupPlayer() {
+        guard let audioPath = audioPath else {return}
+
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+            print(player.url)
+        } catch {
+            print("Error", error.localizedDescription)
+        }
     }
 
     @IBAction func sliderVolumeChanged(_ sender: Any) {
