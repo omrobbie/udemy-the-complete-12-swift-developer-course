eb60dba173278248f6be094cd731a646a41aeb1b
omrobbie <putu.roby@yahoo.com>
2020-08-24 12:45:22 +0800

    add swipe gesture

--- ShakeAndSwipeExample/ShakeAndSwipeExample/ViewController.swift
+++ ShakeAndSwipeExample/ShakeAndSwipeExample/ViewController.swift
@@ -14,14 +14,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        swipeObserver()
     }
 
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEvent.EventSubtype.motionShake {
+            lblTitle.textAlignment = .center
             lblTitle.transform = CGAffineTransform(translationX: 20, y: 0)
             UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
                 self.lblTitle.transform = CGAffineTransform.identity
             }, completion: nil)
+        }
+    }
+
+    private func swipeObserver() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSswiped(handle:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSswiped(handle:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+    }
+
+    @objc private func handleSswiped(handle: UIGestureRecognizer) {
+        guard let handle = handle as? UISwipeGestureRecognizer else {return}
+
+        switch handle.direction {
+        case .right: lblTitle.textAlignment = .right
+        case .left: lblTitle.textAlignment = .left
+        default: break
         }
     }
 }
