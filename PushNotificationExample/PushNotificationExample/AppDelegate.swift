50e55e0a77b458884124cc8ed36866153864f187
omrobbie <putu.roby@yahoo.com>
2020-08-25 16:05:19 +0800

    setup user notification center

--- PushNotificationExample/PushNotificationExample/AppDelegate.swift
+++ PushNotificationExample/PushNotificationExample/AppDelegate.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 import Firebase
+import FirebaseInstanceID
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        notificationConfigure(application: application)
+
+        InstanceID.instanceID().instanceID(handler: { (result, _) in
+            if let token = result?.token {
+                print("Token", token)
+            }
+        })
         return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    private func notificationConfigure(application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOption: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOption) { (_, _) in
+
+        }
+
+        application.registerForRemoteNotifications()
     }
 }
