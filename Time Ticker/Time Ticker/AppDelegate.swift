af3be5632fc000ce53df8ccecc8601171b5d01bc
omrobbie <putu.roby@yahoo.com>
2020-08-27 16:39:53 +0800

    clean up

--- Time Ticker/Time Ticker/AppDelegate.swift
+++ Time Ticker/Time Ticker/AppDelegate.swift
@@ -11,49 +11,17 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    // MARK: - Core Data stack
-
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
         let container = NSPersistentContainer(name: "Time_Ticker")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error)")
             }
         })
         return container
     }()
 
-    // MARK: - Core Data Saving and Undo support
-
     @IBAction func saveAction(_ sender: AnyObject?) {
-        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
         let context = persistentContainer.viewContext
 
         if !context.commitEditing() {
@@ -63,7 +31,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             do {
                 try context.save()
             } catch {
-                // Customize this code block to include application-specific recovery steps.
                 let nserror = error as NSError
                 NSApplication.shared.presentError(nserror)
             }
@@ -71,12 +38,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func windowWillReturnUndoManager(window: NSWindow) -> UndoManager? {
-        // Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
         return persistentContainer.viewContext.undoManager
     }
 
     func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-        // Save changes in the application's managed object context before the application terminates.
         let context = persistentContainer.viewContext
         
         if !context.commitEditing() {
@@ -93,7 +58,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         } catch {
             let nserror = error as NSError
 
-            // Customize this code block to include application-specific recovery steps.
             let result = sender.presentError(nserror)
             if (result) {
                 return .terminateCancel
@@ -114,9 +78,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 return .terminateCancel
             }
         }
-        // If we got here, it is time to quit.
         return .terminateNow
     }
-
 }
-
