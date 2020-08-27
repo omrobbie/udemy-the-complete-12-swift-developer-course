ec2e499a9f94b59c5ac113669334ab95a8d195a1
omrobbie <putu.roby@yahoo.com>
2020-08-27 16:38:31 +0800

    * start time ticker project *

---
+++ Time Ticker/Time Ticker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Time Ticker
+//
+//  Created by omrobbie on 27/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
