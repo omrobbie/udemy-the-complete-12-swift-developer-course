0abeb07405bd9ba96d8c871393eea75011a35599
omrobbie <putu.roby@yahoo.com>
2020-08-28 15:33:13 +0800

    create table and row for history screen

---
+++ TimeTracker/TimeTracker WatchKit Extension/HistoryInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryInterfaceController.swift
+//  TimeTracker WatchKit Extension
+//
+//  Created by omrobbie on 28/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import WatchKit
+
+class HistoryInterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var table: WKInterfaceTable!
+}
---
+++ TimeTracker/TimeTracker WatchKit Extension/HistoryRow.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryRow.swift
+//  TimeTracker WatchKit Extension
+//
+//  Created by omrobbie on 28/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import WatchKit
+
+class HistoryRow: NSObject {
+
+    @IBOutlet weak var label: WKInterfaceLabel!
+}
